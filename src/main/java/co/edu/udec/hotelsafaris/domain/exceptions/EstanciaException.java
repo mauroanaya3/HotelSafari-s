ed70b693cd7c8d672f3bbaf8e238703425ffe8f3
Mauro Anaya <mauroanaya13@gmail.com>
2025-09-30 21:58:22 -0500

    Creacion de Exepciones (EstanciaException)

---
+++ src/main/java/co/edu/udec/hotelsafaris/domain/exceptions/EstanciaException.java
@@ -0,0 +1,7 @@
+package co.edu.udec.hotelsafaris.domain.exceptions;
+
+public class EstanciaException extends RuntimeException {
+    public EstanciaException(String message) {
+        super(message);
+    }
+}
